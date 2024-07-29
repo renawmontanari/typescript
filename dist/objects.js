5b91067ab281ce5d70e582087ff266a6bcd389b3
renawmontanari <regiangelica@gmail.com>
2024-07-29 19:30:18 -0300

    objects

---
+++ dist/objects.js
@@ -0,0 +1,31 @@
+"use strict";
+const user = {
+    id: 1,
+    firstName: 'Jane',
+    age: 40,
+    email: 'jane@gmail.com',
+    password: '123456',
+    orders: [{ productId: "1", price: 200 }],
+};
+const printLog = (message) => { };
+printLog(user.password);
+const author = {
+    bookId: ['1'],
+    books: ['books', 'books'],
+    category: ['Fiction', 'Action'],
+    id: 1,
+    firstName: 'Jane',
+    age: 40,
+    email: 'jane@gmail.com',
+    password: '123456',
+    orders: [{ productId: "1", price: 200 }],
+};
+const emailUser = {
+    email: 'jane@jane.com',
+    firstName: 'Jane'
+};
+const newAuthor = {
+    email: 'jane@gmail.com',
+    firstName: 'Jane',
+    books: ['books', 'books']
+};
--- src/objects.ts
+++ src/objects.ts
@@ -28,3 +28,41 @@ const printLog = (message: string) => {}
 printLog(user.password!);
 
 // Unions
+type Author = {
+  bookId: string[];
+  books: string[];
+  category: string[];
+}
+
+const author: Author & User = {
+  bookId: ['1'],
+  books: ['books', 'books'],
+  category: ['Fiction', 'Action'],
+  id: 1,
+  firstName: 'Jane',
+  age: 40,
+  email: 'jane@gmail.com',
+  password: '123456',
+  orders: [{ productId: "1", price: 200 }],
+}
+
+// Interfaces
+interface UserInterface {
+  readonly firstName: string,
+  email: string
+}
+
+const emailUser: UserInterface = {
+  email: 'jane@jane.com',
+  firstName: 'Jane'
+}
+
+interface AuthorInterface {
+  books: string[];
+}
+
+const newAuthor: UserInterface & AuthorInterface = {
+  email: 'jane@gmail.com',
+  firstName: 'Jane',
+  books: ['books', 'books'],
+}
